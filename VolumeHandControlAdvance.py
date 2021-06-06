89aed7dc596eda1a70b82686b647d5ebd6012f6c
hichemseriket <serikethichem@hotmail.fr>
2021-06-07 01:21:31 +0200

    Initial commit je push tout les codes du tracking avant formatage du pc jamais push

---
+++ VolumeHandControlAdvance.py
@@ -0,0 +1,97 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+#########################################
+wCam, hCam = 640, 480
+#########################################
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.7, maxHands=1)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+# volume.GetVolumeRange()
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+area = 0
+colorVol = (255, 0, 0)
+while True:
+    success, img = cap.read()
+
+    # Find Hand
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img, draw=True)
+    if len(lmList) != 0:
+
+        # Filter based on size
+        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+        # wB, hB = bbox[2]-bbox[0], bbox[3]-bbox[1]
+        # print(area)
+        if 250 < area < 1000:
+
+            # Find distance between index and pouce
+            length, img, lineInfo =detector.findDistance(4, 8, img)
+            # print(length)
+
+            # Convertir le volume
+            # Hand range de 50 à 200 a distance de 1m plus proche de la cam la distance entre les doight devient 300
+            # Volume Range de -65 à 0 depuis la library pycaw
+            volBar = np.interp(length, [50, 180], [400, 150])
+            volPer = np.interp(length, [50, 180], [0, 100])
+
+            # Reduire la resolution pour que cela devient smoother
+            smoothness = 10
+            volPer = smoothness * round(volPer/smoothness)
+
+            # Check finger up
+            fingers = detector.fingersUp()
+            # print(fingers)
+            # If pinky " petit doight"  is down set volume
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer/100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 10, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
+                # time.sleep(0.25)
+            else:
+                colorVol = (255, 0, 0)
+
+    # Drawings
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    # je mets la hauteur a vol
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    cVol = int(volume.GetMasterVolumeLevelScalar()*100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
+
+    # Frame rate
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    cv2.imshow("hichem", img)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
